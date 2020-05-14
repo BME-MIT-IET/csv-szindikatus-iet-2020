2560397a3f490ee33820ef73f5d09cd6a57c4484
cynolterm <cynolter.marton@gmail.com>
2020-05-14 11:42:47 +0200

    RowValueProviderTest

--- src/test/java/com/complexible/common/csv/ProviderTest.java
+++ src/test/java/com/complexible/common/csv/ProviderTest.java
@@ -7,14 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ProviderTest {
 
+    private String[] array = new String[]{"Hello", "this", "is", "a", "test"};
+    private int rowIndex = 1;
+
+    /**
+     * Tests the RowNumberProvider subclass of the Provider abstract class
+     */
     @Test
     public void RowNumberProviderTest(){
         RowNumberProvider rnp = new RowNumberProvider();
-        String[] array = new String[]{"Hello", "this", "is", "a", "test"};
-        int rowIndex = 1;
-        rnp.setHashed(true);
         String result = rnp.provide(rowIndex, array);
-        assertEquals(result, array[1]);
+        assertEquals(result, String.valueOf(rowIndex));
     }
 
+    /**
+     * Tests the RowValueProvider subclass of the Provider abstract class
+     */
+    @Test
+    public void RowValueProviderTest(){
+        RowValueProvider rvp = new RowValueProvider(2);
+        String result = rvp.provide(rowIndex, array);
+        assertEquals(result, array[2]);
+    }
 }
