36693a485b514b1f63b6e0bc621afbf8c7497f3f
cynolterm <cynolter.marton@gmail.com>
2020-05-14 12:50:38 +0200

    TemplateURIGeneratorWithMoreProvidersTest

--- src/test/java/com/complexible/common/csv/GeneratorTest.java
+++ src/test/java/com/complexible/common/csv/GeneratorTest.java
@@ -11,24 +11,45 @@ import org.openrdf.model.URI;
 import org.openrdf.model.ValueFactory;
 import org.openrdf.model.impl.ValueFactoryImpl;
 
+import javax.xml.stream.FactoryConfigurationError;
+
 public class GeneratorTest {
+
+    private int rowIndex = 1;
+    private String[] array = new String[]{"Hello","this","is","a","test"};
+    private ValueFactory FACTORY = ValueFactoryImpl.getInstance();
 
     /**
      * Tests the TemplateURIGenerator subclass of the TemplateValueGenerator
      */
     @Test
-    public void TemplateURIGeneratorTest(){
+    public void TemplateURIGeneratorWithRowNumberProviderTest(){
         ValueProvider[] providers = new ValueProvider[1];
         RowNumberProvider rnp = new RowNumberProvider();
         providers[0] = rnp;
         String ph = rnp.getPlaceholder();
         String template = "http://" + ph + "hello" + ph + ph + ".com";
-        int rowIndex = 1;
-        String[] array = new String[]{"Hello","this","is","a","test"};
         TemplateURIGenerator tug = new TemplateURIGenerator(template, providers);
         URI uri = tug.generate(rowIndex, array);
         String expectedString = "http://1hello11.com";
-        ValueFactory FACTORY = ValueFactoryImpl.getInstance();
+        URI expectedURI = FACTORY.createURI(expectedString);
+        assertEquals(true, expectedURI.equals(uri));
+    }
+
+    /**
+     * Tests if the TemplateURIGenerator functions well with more than one given provider
+     */
+    @Test
+    public void TemplateURIGeneratorWithMoreProvidersTest(){
+        ValueProvider[] providers = new ValueProvider[2];
+        RowNumberProvider rnp = new RowNumberProvider();
+        RowValueProvider rvp = new RowValueProvider(1);
+        String rnpPH = rnp.getPlaceholder();
+        String rvpPH = rvp.getPlaceholder();
+        String template = "http://" + rnpPH + rvpPH + "hello" + rnpPH + ".com";
+        String expectedString = "http://1thishello1.com";
+        TemplateURIGenerator tug = new TemplateURIGenerator(template, providers);
+        URI uri = tug.generate(rowIndex, array);
         URI expectedURI = FACTORY.createURI(expectedString);
         assertEquals(true, expectedURI.equals(uri));
     }
