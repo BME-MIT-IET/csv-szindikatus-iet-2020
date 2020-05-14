162ba1bd27cebd9fe41c53e69579f538bfaac206
cynolterm <cynolter.marton@gmail.com>
2020-05-14 11:25:53 +0200

    RowNumberProviderTest

---
+++ src/test/java/com/complexible/common/csv/ProviderTest.java
@@ -0,0 +1,19 @@
+package com.complexible.common.csv;
+
+import org.junit.Test;
+import com.complexible.common.csv.provider.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ProviderTest {
+
+    @Test
+    public void RowNumberProviderTest(){
+        RowNumberProvider rnp = new RowNumberProvider();
+        String[] array = new String[]{"Hello", "this", "is", "a", "test"};
+        int rowIndex = 1;
+        String result = rnp.provide(rowIndex, array);
+        assertEquals(result, array[1]);
+    }
+
+}
