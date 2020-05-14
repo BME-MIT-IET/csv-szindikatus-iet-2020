a2e8284c5d14c96dfcc2e2cbbe0109501c9d2b17
cynolterm <cynolter.marton@gmail.com>
2020-05-14 13:39:24 +0200

    .

--- src/test/java/com/complexible/common/csv/GeneratorTest.java
+++ src/test/java/com/complexible/common/csv/GeneratorTest.java
@@ -64,7 +64,7 @@ public class GeneratorTest {
      */
     @Test
     public void TemplateLiteralGeneratorTest(){
-        Literal literal = new LiteralImpl();
+        Literal literal = new LiteralImpl("testLabel", "testLanguage");
         String template = literal.getLabel();
         ValueProvider[] providers = new ValueProvider[1];
         RowNumberProvider rnp = new RowNumberProvider();
