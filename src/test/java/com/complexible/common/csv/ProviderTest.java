b70597343b277c4ba1887650ab064e79f6124560
tmsbrndz <31684353+tmsbrndz@users.noreply.github.com>
2020-05-17 14:57:41 +0200

    ProviderTest refactored

--- src/test/java/com/complexible/common/csv/ProviderTest.java
+++ src/test/java/com/complexible/common/csv/ProviderTest.java
@@ -4,39 +4,43 @@ import org.junit.Test;
 import com.complexible.common.csv.provider.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class ProviderTest {
-
-    private String[] array = new String[]{"Hello", "this", "is", "a", "test"};
-    private int rowIndex = 1;
+    private final String[] array = new String[] { "Hello", "this", "is", "a", "test" };
+    private final int rowIndex = 1;
 
     /**
      * Tests the RowNumberProvider subclass of the Provider abstract class
      */
     @Test
-    public void RowNumberProviderTest(){
-        RowNumberProvider rnp = new RowNumberProvider();
-        String result = rnp.provide(rowIndex, array);
-        assertEquals(result, String.valueOf(rowIndex));
+    public void RowNumberProviderTest() {
+        RowNumberProvider rowNumberProvider = new RowNumberProvider();
+        String result = rowNumberProvider.provide(rowIndex, array);
+        String rowIndexToString = String.valueOf(rowIndex);
+
+        assertEquals(result, rowIndexToString);
     }
 
     /**
      * Tests the RowValueProvider subclass of the Provider abstract class
      */
     @Test
-    public void RowValueProviderTest(){
-        RowValueProvider rvp = new RowValueProvider(2);
-        String result = rvp.provide(rowIndex, array);
-        assertEquals(result, array[2]);
+    public void RowValueProviderTest() {
+        final int colIndex = 2;
+        RowValueProvider rowValueProvider = new RowValueProvider(colIndex);
+        String result = rowValueProvider.provide(rowIndex, array);
+        String expectedResult = array[colIndex];
+
+        assertEquals(expectedResult, result);
     }
 
     /**
      * Tests the UUIDProvider subclass of the Provider abstract class
      */
     @Test
-    public void UUIDProviderTest(){
-        UUIDProvider up = new UUIDProvider();
-        String result = up.provide(rowIndex, array);
-        assertNotEquals(null, result);
+    public void UUIDProviderTest() {
+        UUIDProvider uuidProvider = new UUIDProvider();
+        String result = uuidProvider.provide(rowIndex, array);
+
+        assertNotNull(result);
     }
 }
